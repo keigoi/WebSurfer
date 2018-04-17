a62272af3a91a2a6f355cf60b436b35fab34ef0b
keigoi <keigoi@gifu-u.ac.jp>
2018-04-18 01:11:14 +0900

    initial commit

---
+++ app/src/main/java/jp/ac/gifu_u/info/lec/prog2/websurfer/Downloader.java
@@ -0,0 +1,79 @@
+package jp.ac.gifu_u.info.lec.prog2.websurfer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class Downloader {
+
+    // ダウンロードした文字列を保持する。
+    private String content = null;
+
+    // ダウンロード禁止ドメインのリストを保持する。
+    private ArrayList<String> bannedDomains = new ArrayList<>();
+
+    /**
+     * 直前のダウンロードが成功していればダウンロードした内容を、失敗していれば null を返す
+     * @return ダウンロードした文字列
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * 禁止ドメインを追加する。
+     * @param domain 追加するドメイン
+     */
+    public void addBannedDomain(String domain) {
+        // TODO ここを埋める
+    }
+
+    /**
+     * 禁止ドメインを削除する。
+     * @param domain 削除するドメイン
+     */
+    public void removeBannedDomain(String domain) {
+        // TODO ここを埋める
+    }
+
+    /**
+     * 与えられた URL からファイルをダウンロードする
+     * @param urlstr URL文字列
+     * @return ダウンロードに成功すれば true, 禁止されたドメインであれば false
+     * @throws IOException ダウンロード途中でエラーが発生した場合
+     */
+    public boolean download(String urlstr) throws IOException {
+        URL url = new URL(urlstr);
+
+        // TODO url.getHost() が bannedDomains のどれかで終わるときに false を返す処理を入れる
+        // url.getHost().endsWith(禁止文字列)
+
+        // URL を設定して HTTP 通信を開始(メソッドは GET)
+        HttpURLConnection http = (HttpURLConnection) url.openConnection();
+        http.setRequestMethod("GET");
+        http.connect();
+
+        // 受信したデータを読み出すためのストリームをオープン
+        StringBuffer text = new StringBuffer();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(http.getInputStream(), "UTF-8"));
+
+        // ストリームからデータを読み出し
+        while (true) {
+            String str = reader.readLine();
+            if (str == null) {
+                break;
+            } else {
+                text.append(str);
+            }
+        }
+
+        this.content = text.toString();
+        return true;
+    }
+
+}
---
+++ app/src/test/java/jp/ac/gifu_u/info/lec/prog2/websurfer/DownloaderTest.java
@@ -0,0 +1,63 @@
+package jp.ac.gifu_u.info.lec.prog2.websurfer;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+/**
+ * Downloader クラスのユニットテスト
+ * Created by keigoi on 2017/05/30.
+ */
+public class DownloaderTest {
+
+    /**
+     * プロキシ設定（岐阜大学内専用...）
+     */
+    @Before
+    public void setUp() {
+        Properties systemProperties = System.getProperties();
+        systemProperties.setProperty("http.proxyHost", "proxy.eng.gifu-u.local");
+        systemProperties.setProperty("http.proxyPort", "10080");
+    }
+    /*
+     * 何も前提がなければダウンロードが成功することを確認するテスト
+     */
+    @Test
+    public void download_success() throws Exception {
+        Downloader d = new Downloader();
+
+        // download メソッドが true を返し、getContent は 非 null のはず
+        assertTrue( d.download("http://example.com/") );
+        assertNotNull( d.getContent() );
+    }
+
+    /*
+     * 禁止された URL で download() が false を返すことを確認するテスト
+     */
+    @Test
+    public void addBanned_download_banned() throws Exception {
+        Downloader d = new Downloader();
+
+        d.addBannedDomain("example.com");
+
+        assertFalse( d.download("http://example.com/index.html") );
+        assertNull( d.getContent() );
+
+        assertFalse( d.download("http://bad.example.com/index.html") );
+        assertNull( d.getContent() );
+    }
+
+    /*
+     * removeBanned で禁止 URL から外れることを確認するテスト
+     */
+    @Test
+    public void removeBanned_download_success() throws Exception {
+        // TODO テストを書く。ダウンロード URL は "http://example.com/" 等を使う
+    }
+
+}
